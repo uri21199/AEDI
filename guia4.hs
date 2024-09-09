25575d6b8e7c6a3cf5e48e86c6569aa3dc918fc6
Lautaro Bustos <86994324+uri21199@users.noreply.github.com>
2024-09-09 18:44:22 -0300

    Update guia4.hs

--- guia4.hs
+++ guia4.hs
@@ -7,28 +7,30 @@ fibonacci n = fibonacci(n-1)+fibonacci(n-2)
 -- Ej 2
 parteEntera :: Float -> Int
 parteEntera n | n >= 0 && n < 1 = 0
-              | n > 1 = parteEntera (n-1) + 1 
+    | n > 1 = parteEntera (n-1) + 1 
 
 -- Ej 3
 esDivisible :: Int -> Int -> Bool
-esDivisible x y | x - y == 0 = True 
-                | x - y < 0 = False
-                | otherwise = esDivisible (x - y) y
+esDivisible x y | x - y == 0 = True
+    | x - y < 0 = False
+    | otherwise = esDivisible (x - y) y
 
 -- Ej 4
 sumaImpares :: Int -> Int
-sumaImpares | 1 = 1
-            | sumaImpares (n-1)
+sumaImpares n | n <= 0 = 0
+    | otherwise = 2 * (n - 1) + 1 + sumaImpares (n - 1)
+
 
 -- Ej 5
-medioFact :: Int -> Int
-medioFact | n >= 0 = 
+--medioFact :: Int -> Int
+--medioFact | n >= 0 = 
 
 
 -- Ej de clase
 cantDigitos :: Int -> Int
-cantDigitos | n <= 10 = 1
-            | otherwise = cantDigitos(div n 10) + 1
+cantDigitos n | n <= 10 = 1
+    | otherwise = cantDigitos(div n 10) + 1
 
-iesimoDigito :: Int -> Int
-iesimoDigito |
+iesimoDigito :: Int -> Int -> Int
+iesimoDigito n m | cantDigitos n == m = mod n 10
+                 | otherwise = iesimoDigito (div n 10) m
